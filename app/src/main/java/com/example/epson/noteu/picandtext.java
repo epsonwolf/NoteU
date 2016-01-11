435c5897b64441ca8b0ff3bd64eac717379c3770
epsonwolf <ms0239835@gmail.com>
2016-01-12 05:04:54 +0800

    final GG

--- app/src/main/java/com/example/epson/noteu/picandtext.java
+++ app/src/main/java/com/example/epson/noteu/picandtext.java
@@ -3,15 +3,19 @@ package com.example.epson.noteu;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+
+import java.io.FileNotFoundException;
 
 /**
  * Created by epson on 2016/1/10.
@@ -63,11 +67,28 @@ public class picandtext extends AppCompatActivity {
         if (resultCode == RESULT_OK && requestCode == 0) {
             ContentResolver resolver = getContentResolver();
             Uri originalUri = data.getData();
-            String temp = originalUri.toString();
-            int x = Integer.valueOf(temp);
-            Drawable pic = getResources().getDrawable(x);
+            String temp = edtMix.getText().toString();
+            //ImageView img = new ImageView();
+            //int x = Integer.valueOf(temp);
+            //Drawable pic = img.getDrawable();
             SpannableString spnString = new SpannableString(temp);
+            try {
+                bitmap = BitmapFactory.decodeStream(resolver.openInputStream(originalUri));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
             ImageSpan imageSpan = new ImageSpan(picandtext.this,bitmap);
+
+            spnString.setSpan(imageSpan, 0, temp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            // 將圖片追加到EditText中光光標所在位置
+            int index = edtMix.getSelectionStart(); // 光標所在位置
+            Editable edit_text = edtMix.getEditableText();
+            if (index < 0 || index >= edit_text.length()) {
+                edit_text.append(spnString);
+            } else {
+                edit_text.insert(index, spnString);
+            }
+            System.out.println(spnString.toString());
         }
     }
 }
--- app/src/main/java/com/example/epson/noteu/simpletxt.java
+++ app/src/main/java/com/example/epson/noteu/simpletxt.java
@@ -3,7 +3,6 @@ package com.example.epson.noteu;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -15,15 +14,10 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by epson on 2016/1/8.
@@ -45,10 +39,59 @@ public class simpletxt extends AppCompatActivity {
         btnOld.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("*/*");
-                startActivityForResult(intent, 0);
+                intent.setType("*i/*");
+                startActivityForResult(intent, 0);*/
+                final EditText input = new EditText(simpletxt.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                new AlertDialog.Builder(simpletxt.this)
+                        .setTitle("請選擇開啟檔案")
+                        .setMessage("輸入檔名")
+                        .setView(input)
+                        .setPositiveButton("讀檔", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                filename = input.getText().toString();
+                                try
+                                {
+                                    //取得SD卡儲存路徑
+                                    File mSDFile = Environment.getExternalStorageDirectory();
+                                    //讀取文件檔路徑
+                                    FileReader mFileReader = new FileReader(mSDFile.getParent() + "/" + mSDFile.getName() + "/NoteU_txt/"+filename+".txt");
+
+                                    BufferedReader mBufferedReader = new BufferedReader(mFileReader);
+                                    String mReadText = "";
+                                    String mTextLine = mBufferedReader.readLine();
+
+                                    //一行一行取出文字字串裝入String裡，直到沒有下一行文字停止跳出
+                                    while (mTextLine!=null)
+                                    {
+                                        mReadText += mTextLine+"\n";
+                                        mTextLine = mBufferedReader.readLine();
+                                    }
+                                    //文字放入txt1裡，並清空mEdit
+                                    txt1.setText(mReadText);
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                    new AlertDialog.Builder(simpletxt.this)
+                                            .setMessage("哈哈哈哈，你連你存過什麼檔都忘記，太不應該了╮(╯▽╰)╭")
+                                            .show();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }).show();
             }
         });
 
@@ -98,17 +141,13 @@ public class simpletxt extends AppCompatActivity {
                                 try {
                                     File mSDFile = null;
                                     mSDFile = Environment.getExternalStorageDirectory();
-                                    File mFile = new File(mSDFile.getParent() + "/" + mSDFile.getName() + "/NoteU_txt/" + filename + ".txt");
+                                    File mFile = new File(mSDFile.getParent() + "/" + mSDFile.getName() + "/NoteU_txt");
                                     if (!mFile.exists()) {
                                         mFile.mkdirs();
                                     }
-                                    mFile.createNewFile();
-                                    FileOutputStream fOut = new FileOutputStream(mFile);
-                                    OutputStreamWriter myOutWriter =
-                                            new OutputStreamWriter(fOut);
-                                    myOutWriter.append(txt1.getText());
-                                    myOutWriter.close();
-                                    fOut.close();
+                                    FileWriter mFileWriter = new FileWriter(mSDFile.getParent() + "/" + mSDFile.getName() + "/NoteU_txt/" + filename + ".txt");
+                                    mFileWriter.write(txt1.getText().toString());
+                                    mFileWriter.close();
                                     Toast.makeText(simpletxt.this, "已儲存檔案", Toast.LENGTH_SHORT).show();
                                     Intent data = new Intent();
                                     data.setClass(simpletxt.this, MainActivity.class);
@@ -130,7 +169,7 @@ public class simpletxt extends AppCompatActivity {
         });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
             Uri uri = data.getData();
@@ -138,12 +177,12 @@ public class simpletxt extends AppCompatActivity {
                 try {
                     InputStreamReader isr = new InputStreamReader(new FileInputStream(uri.getPath()), "Big5");
                     BufferedReader br = new BufferedReader(isr);
-                    List<String> cont  = new ArrayList<String>();
-                    String line ="";
-                    while((line = br.readLine()) != null) {
-                        cont.add(line);
-                    }
-                    txt1.setText((CharSequence) cont);
+                    byte[] bufBytes = new byte[10];
+                    txt1.setText("");
+                    do{
+                        int c = br.read(bufBytes);
+
+                    }while(true);
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 } catch (FileNotFoundException e) {
@@ -159,6 +198,6 @@ public class simpletxt extends AppCompatActivity {
         else{
             setTitle("取消選擇檔案 !!");
         }
-    }
+    }*/
 
 }
